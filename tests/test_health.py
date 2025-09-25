0876494b347599482dcc0aa32fd427d96a050825
NahueAdema <ademanahuel@gmail.com>
2025-09-24 23:14:45 -0300

    arreglo de modulos

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,4 +1,7 @@
 from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import app
 
 client = TestClient(app)
